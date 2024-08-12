b5e10262a8a3dabed8fb95015b89dd5c071b3d02
HaeSoo <gumake999@gmail.com>
2024-08-12 16:42:19 +0900

    FEAT: Add Search Input Box

---
+++ src/stories/SearchInput/SearchInput.stories.ts
@@ -0,0 +1,55 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+
+import { SearchInput } from '../../packages';
+
+import IconSearch from '../../asset/icons/defaultSearchIcon.svg';
+
+const meta: Meta<typeof SearchInput> = {
+  title: '검색박스/SearchInput',
+  component: SearchInput,
+  argTypes: {
+    label: {
+      control: {
+        type: 'text',
+      },
+      description: 'label 영역 텍스트 입력'
+    },
+    description: {
+      control: {
+        type: 'text',
+      },
+      description: 'description 영역 텍스트 입력'
+    },
+    inputWidth: {
+      control: {
+        type: 'text',
+      },
+      description: 'inputWidth 크기 입력(px)'
+    },
+    placeholder: {
+      control: {
+        type: 'text',
+      },
+      description: 'placeholder 영역 텍스트 입력'
+    },
+    searchIconUrl: {
+      control: {
+        type: 'text',
+      },
+      description: '검색 아이콘 url 입력',
+    },
+  },
+};
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    label: 'Label',
+    description: 'Description',
+    searchIconUrl: IconSearch,
+    searchButtonOnClick: fn(() => window.alert('button click')),
+  },
+};
