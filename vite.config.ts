718f6653cfd011cc8dd5e057b5d91f42e418dec0
HaeSoo <gumake999@gmail.com>
2024-10-21 21:11:26 +0900

    FIX: Change scripts option

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,16 @@
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
 export default defineConfig({
-    resolve: {
-        alias: {
-            '@packages': resolve(__dirname, 'src/packages')
-        }
-    },
     plugins: [
         react(),
         tsconfigPaths(),
     ],
-    build: {
-        lib: {
-            entry: 'src/index.ts',
-            name: 'social-food-ui',
-            fileName: 'index',
-            formats: ['es', 'cjs']
-        },
-        rollupOptions: {
-            external: ['@emotion/react', '@emotion/styled']
+    resolve: {
+        alias: {
+            "@packages": resolve(__dirname, "./src/packages"),
         },
     },
-    optimizeDeps: {
-        exclude: ['@emotion/react', '@emotion/styled']
-    }
 });
